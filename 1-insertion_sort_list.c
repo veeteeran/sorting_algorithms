3e4d37d100a99383cddec838b378a6b4b4c4918b
Viet Tran <vietnamtran@gmail.com>
2020-06-15 19:01:56 +0000

    Appease betty

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,5 +1,5 @@
 #include "sort.h"
-
+void reset_ptr(listint_t *element, listint_t *mover);
 /**
  * insertion_sort_list - sorts a doubly linked list using insertion sort
  * @list: list to sort
@@ -30,29 +30,38 @@ void insertion_sort_list(listint_t **list)
 		}
 		while (element->n < mover->n && mover->prev != NULL && element->prev != NULL)
 		{
-		  mover = mover->prev;
-		  element->prev->next = element->next;
-		  if (element->next != NULL)
-		    element->next->prev = element->prev;
-		  element->prev = mover;
-		  element->next = mover->next;
-		  mover->next->prev = element;
-		  mover->next = element;
-		  print_list(*list);
+			mover = mover->prev;
+			element->prev->next = element->next;
+			if (element->next != NULL)
+				element->next->prev = element->prev;
+			element->prev = mover;
+			element->next = mover->next;
+			mover->next->prev = element;
+			mover->next = element;
+			print_list(*list);
 		}
-		if (mover->prev == NULL && element->n < mover->n)                                                                                        
-		  {                                                                                                                                         
-		    mover->prev = element;                                                                                                                  
-		    element->prev->next = element->next;                                                                                                    
-		    element->next->prev = element->prev;                                                                                                    
-		    element->prev = NULL;                                                                                                                   
-		    element->next = mover;                                                                                                                  
-		    *list = element;
-		    print_list(*list);
-		  }
-
+		if (mover->prev == NULL && element->n < mover->n)
+		{
+			reset_ptr(element, mover);
+			*list = element;
+			print_list(*list);
+		}
 		mover = sorted_end;
 		if (sorted_end->next != NULL)
 			element = sorted_end->next;
 	}
+}
+
+/**
+ * reset_ptr - point nodes in list properly
+ * @element: the element to compare
+ * @mover: pointer moving through list
+ */
+void reset_ptr(listint_t *element, listint_t *mover)
+{
+	mover->prev = element;
+	element->prev->next = element->next;
+	element->next->prev = element->prev;
+	element->prev = NULL;
+	element->next = mover;
 }
