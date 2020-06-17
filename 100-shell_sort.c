6bffffe5d9850a69a6afe0fa9aa1c7e4a38926f9
Viet Tran <vietnamtran@gmail.com>
2020-06-17 20:34:49 +0000

    Betty complianace

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -6,29 +6,27 @@
  * @size: size of array
  */
 
-void shell_sort(int *array, size_t size) 
+void shell_sort(int *array, size_t size)
 {
-  size_t knuth, c, c2;
-  int temp;
-    
-  knuth = 1;
-  while (knuth < size)
-    knuth = (knuth * 3) + 1;
-  knuth = (knuth - 1) / 3;
-  while (knuth > 0)
-    {
-      for (c = knuth; c < size; c++) 
-	{
-	  temp = array[c];
-	  for (c2 = c; c2 >= knuth && array[c2 - knuth] > temp; c2 -= knuth) 
-	    {
-	      array[c2] = array[c2 - knuth];
-	    }
-	  array[c2] = temp;
-	}
-      knuth = (knuth - 1) / 3;
-      print_array(array, size);
-	
-    }
-}
+	size_t knuth, c, c2;
+	int temp;
 
+	knuth = 1;
+	while (knuth < size)
+		knuth = (knuth * 3) + 1;
+	knuth = (knuth - 1) / 3;
+	while (knuth > 0)
+	{
+		for (c = knuth; c < size; c++)
+		{
+			temp = array[c];
+			for (c2 = c; c2 >= knuth && array[c2 - knuth] > temp; c2 -= knuth)
+			{
+				array[c2] = array[c2 - knuth];
+			}
+			array[c2] = temp;
+		}
+		knuth = (knuth - 1) / 3;
+		print_array(array, size);
+	}
+}
